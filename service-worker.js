21fe5a4759c2196075fa0e5781c5f2e65f866d21
marcusoji <165146879+marcusoji@users.noreply.github.com>
2025-08-29 19:08:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,42 @@
-self.addEventListener("install", event => {
-      event.waitUntil(
-            caches.open("todo-cache").then(cache => {
-                      return cache.addAll(["/", "/index.html", "/index.css", "/index.js"]);
-            })
-      );
-});
+const CACHE_NAME = "todo-cache-v1";
+const urlsToCache = [
+        "/",               // root
+          "/index.html",     // main html
+            "/index.css",      // your css
+              "/index.js",       // your js
+                "/manifest.json",  // manifest
+];
 
-self.addEventListener("fetch", event => {
-      event.respondWith(
-            caches.match(event.request).then(response => {
-                      return response || fetch(event.request);
-            })
-      );
-});
+// Install Service Worker and cache files
+self.addEventListener("install", (event) => {
+        event.waitUntil(
+                caches.open(CACHE_NAME).then((cache) => {
+                        return cache.addAll(urlsToCache);
+                })
+            );
+      });
+
+      // Serve files from cache first
+      self.addEventListener("fetch", (event) => {
+              event.respondWith(
+                      caches.match(event.request).then((response) => {
+                              return response || fetch(event.request);
+                      })
+                  );
+            });
+
+            // Update cache when new version is deployed
+            self.addEventListener("activate", (event) => {
+                    event.waitUntil(
+                            caches.keys().then((cacheNames) =>
+                              Promise.all(
+                                            cacheNames.map((name) => {
+                                                          if (name !== CACHE_NAME) {
+                                                                        return caches.delete(name);
+                                                          }
+                                                      })
+                                                )
+                                          )
+                                    );
+                              });
+                                                          
